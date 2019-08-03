7ce59df34cbadbff39fbf4b9b92ec605fe23aa7b
Will <yydragon.000@gmail.com>
2019-08-03 23:19:19 +0800

    Make the multi-projects really be able to run
    Downgrade the spring-boot back to 2.1.6.RELEASE, spring-boot-admin to 2.1.5 to make the projects running.
    For now, spring-boot 2.2.0.M4 cannot work with spring-boot-admin 2.1.6 yet.

--- spring-cloud-in-action/springboot-admin/src/test/java/net/will/sbgradlestudy/SpringBootAdminApplicationTests.java
+++ spring-cloud-in-action/springboot-admin/src/test/java/net/will/sbgradlestudy/SpringBootAdminApplicationTests.java
@@ -1,13 +1,16 @@
 package net.will.sbgradlestudy;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringBootAdminApplicationTests {
+public class SpringBootAdminApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
