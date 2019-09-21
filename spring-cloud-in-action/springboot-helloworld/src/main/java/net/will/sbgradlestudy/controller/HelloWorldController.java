5aad338b372a20fc9eda35eaf85781a262cd1202
Will <yydragon.000@gmail.com>
2019-09-21 14:31:26 +0800

    Some more tests on logging

--- spring-cloud-in-action/springboot-helloworld/src/main/java/net/will/sbgradlestudy/controller/HelloWorldController.java
+++ spring-cloud-in-action/springboot-helloworld/src/main/java/net/will/sbgradlestudy/controller/HelloWorldController.java
@@ -1,17 +1,23 @@
 package net.will.sbgradlestudy.controller;
 
 import net.will.sbgradlestudy.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+    
     @Value("${hello.msg}")
     private String msg;
 
     @RequestMapping("/hello")
     public String hello() {
+        LOGGER.trace("Comming into hello()");
+        
         if (msg != null) {
             throw new BusinessException("1", "This is a business Exception.");
         }
