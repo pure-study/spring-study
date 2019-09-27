2f3b31e57c33d85b66beb0abe23ab09de26fd74c
Will <yydragon.000@gmail.com>
2019-09-27 09:02:37 +0800

    A test project for Feign with parameters in requests

--- spring-cloud-in-action/e-book/e-book-product/e-book-product-api/src/main/java/net/will/ebook/product/facade/ProductFacade.java
+++ spring-cloud-in-action/e-book/e-book-product/e-book-product-api/src/main/java/net/will/ebook/product/facade/ProductFacade.java
@@ -1,8 +1,10 @@
 package net.will.ebook.product.facade;
 
 import net.will.ebook.product.domain.Product;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -12,4 +14,12 @@ public interface ProductFacade {
     @RequestMapping(value = "list", method = RequestMethod.GET)
     public List<Product> listProduct();
     
+    @RequestMapping(value = "get", method = RequestMethod.GET)
+    public Product getProduct(@RequestParam("id") Integer id);
+    
+    @RequestMapping(value = "getComplex", method = RequestMethod.GET)
+    public Product getProductComplex(@RequestParam("id") Integer id, @RequestParam("name") String name);
+    
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public Product addProduct(@RequestBody Product obj);
 }
--- spring-cloud-in-action/e-book/e-book-product/e-book-product-core/src/main/java/net/will/ebook/product/facade/ProductFacadeImpl.java
+++ spring-cloud-in-action/e-book/e-book-product/e-book-product-core/src/main/java/net/will/ebook/product/facade/ProductFacadeImpl.java
@@ -1,6 +1,7 @@
 package net.will.ebook.product.facade;
 
 import net.will.ebook.product.domain.Product;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,27 @@ import java.util.List;
 public class ProductFacadeImpl implements ProductFacade {
     
     @Override
-    @RequestMapping(value = "list", method = RequestMethod.GET)
     public List<Product> listProduct() {
         List<Product> list = new ArrayList<>();
         list.add(new Product(1, "The first eBook Product"));
         list.add(new Product(2, "The second eBook Product"));
         list.add(new Product(3, "The third eBook Product"));
         return list;
+    }
+    
+    @Override
+    public Product getProduct(Integer id) {
+        return new Product(id, "The second eBook Product");
+    }
+    
+    @Override
+    public Product getProductComplex(Integer id, String name) {
+        return new Product(id, name);
+    }
+    
+    @Override
+    public Product addProduct(@RequestBody Product obj) {
+        return obj;
     }
     
 }
