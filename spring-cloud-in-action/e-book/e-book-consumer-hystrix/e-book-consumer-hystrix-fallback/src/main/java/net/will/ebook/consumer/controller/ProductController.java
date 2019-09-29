92f0cd0f58de6404b2050f4c44b8c484a844dbd0
Will <yydragon.000@gmail.com>
2019-09-29 22:13:06 +0800

    Eliminate a redundant domain object

--- spring-cloud-in-action/e-book/e-book-consumer-hystrix/e-book-consumer-hystrix-fallback/src/main/java/net/will/ebook/consumer/controller/ProductController.java
+++ spring-cloud-in-action/e-book/e-book-consumer-hystrix/e-book-consumer-hystrix-fallback/src/main/java/net/will/ebook/consumer/controller/ProductController.java
@@ -1,7 +1,7 @@
 package net.will.ebook.consumer.controller;
 
-import net.will.ebook.consumer.domain.Product;
 import net.will.ebook.consumer.service.ProductService;
+import net.will.ebook.product.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
