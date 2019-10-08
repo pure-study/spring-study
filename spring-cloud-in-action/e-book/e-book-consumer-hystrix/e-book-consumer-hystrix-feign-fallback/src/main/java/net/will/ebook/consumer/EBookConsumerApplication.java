685d9779ac184198233af65d1d1679b967c6963c
Will <yydragon.000@gmail.com>
2019-10-08 15:03:12 +0800

    A new project test for Turbine(test not passed yet)

--- spring-cloud-in-action/e-book/e-book-consumer-hystrix/e-book-consumer-hystrix-feign-fallback/src/main/java/net/will/ebook/consumer/EBookConsumerApplication.java
+++ spring-cloud-in-action/e-book/e-book-consumer-hystrix/e-book-consumer-hystrix-feign-fallback/src/main/java/net/will/ebook/consumer/EBookConsumerApplication.java
@@ -2,14 +2,18 @@ package net.will.ebook.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrixDashboard
 public class EBookConsumerApplication {
     
     public static void main(String[] args) {
