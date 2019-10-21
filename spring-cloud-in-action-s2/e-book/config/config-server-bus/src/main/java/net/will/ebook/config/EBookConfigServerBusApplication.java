346c8ba3c96dfdd685b62f2d7a161f038afc40e0
Will <yydragon.000@gmail.com>
2019-10-21 20:17:00 +0800

    A new test project for spring-cloud-bus
    Test to refresh the configs from config-server.

---
+++ spring-cloud-in-action-s2/e-book/config/config-server-bus/src/main/java/net/will/ebook/config/EBookConfigServerBusApplication.java
@@ -0,0 +1,17 @@
+package net.will.ebook.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class EBookConfigServerBusApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EBookConfigServerBusApplication.class, args);
+    }
+    
+}
