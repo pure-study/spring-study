e8418904065e937675a72e26cf745e1f7e754a61
Will <yydragon.000@gmail.com>
2019-11-05 23:17:29 +0800

    Pass the previous test for Zipkin + RabbitMQ

--- spring-cloud-in-action-s2/e-book/sleuth/sleuth-zipkin-mq-consumer/src/main/java/net/will/ebook/consumer/service/ProductService.java
+++ spring-cloud-in-action-s2/e-book/sleuth/sleuth-zipkin-mq-consumer/src/main/java/net/will/ebook/consumer/service/ProductService.java
@@ -3,6 +3,6 @@ package net.will.ebook.consumer.service;
 import net.will.ebook.product.facade.ProductFacade;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "sleuth-product")
+@FeignClient(name = "sleuth-zipkin-mq-product")
 public interface ProductService extends ProductFacade {
 }
