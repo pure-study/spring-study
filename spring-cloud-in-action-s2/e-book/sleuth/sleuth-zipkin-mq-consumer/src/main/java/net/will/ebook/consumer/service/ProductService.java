90aa02ce3f72b704b94aa06e5a0a6a1f43fbaf2d
Will <yydragon.000@gmail.com>
2019-11-05 08:58:24 +0800

    A group of test projects for Zipkin + RabbitMQ (tests not succeeded yet)

---
+++ spring-cloud-in-action-s2/e-book/sleuth/sleuth-zipkin-mq-consumer/src/main/java/net/will/ebook/consumer/service/ProductService.java
@@ -0,0 +1,8 @@
+package net.will.ebook.consumer.service;
+
+import net.will.ebook.product.facade.ProductFacade;
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "sleuth-product")
+public interface ProductService extends ProductFacade {
+}
